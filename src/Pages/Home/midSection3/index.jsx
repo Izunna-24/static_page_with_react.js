b0e85fe88b6aba5a46085ccf1890d949e259e5e7
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-06-07 11:23:36 +0100

    react is kinda reacting

--- src/Pages/Home/midSection3/index.jsx
+++ src/Pages/Home/midSection3/index.jsx
@@ -6,18 +6,21 @@ const midSection3 = () =>{
     <>
         <div className={style.container}>
             <img src={image} alt="tesla logo" className={style.teslaLogo}/>
-            <p className={style.teslaText}>
-                Maecenas dignissim justo eget nulla rutrum molestie. Maecenas lobortis sem dui, <br/>
-                vel rutrum risus tincidunt ullamcorper. Proin eu enim metus. Vivamus sed libero ornare,<br/>
-                tristique quam in, gravida enim. Nullam ut molestie arcu, at hendrerit elit. Morbi laoreet <br/>
-                elit at ligula molestie, nec molestie mi blandit. Suspendisse cursus tellus sed augue <br/>
-                ultrices, quis tristique nulla sodales. Suspendisse eget lorem eu turpis vestibulum
-                pretium. <br/>
-                Suspendisse potenti. Quisque malesuada enim sapien, vitae placerat ante feugiat eget. <br/>
-                Quisque vulputate odio neque, eget efficitur libero condimentum id. Curabitur id nibh id <br/>
-                sem dignissim finibus ac sit amet magna.
-            </p>
-            <h3 className={style.author}>Tim Smith</h3>
+            <div>
+                <p className={style.teslaText}>
+                    Maecenas dignissim justo eget nulla rutrum molestie. Maecenas lobortis sem dui, <br/>
+                    vel rutrum risus tincidunt ullamcorper. Proin eu enim metus. Vivamus sed libero ornare,<br/>
+                    tristique quam in, gravida enim. Nullam ut molestie arcu, at hendrerit elit. Morbi laoreet <br/>
+                    elit at ligula molestie, nec molestie mi blandit. Suspendisse cursus tellus sed augue <br/>
+                    ultrices, quis tristique nulla sodales. Suspendisse eget lorem eu turpis vestibulum
+                    pretium. <br/>
+                    Suspendisse potenti. Quisque malesuada enim sapien, vitae placerat ante feugiat eget. <br/>
+                    Quisque vulputate odio neque, eget efficitur libero condimentum id. Curabitur id nibh id <br/>
+                    sem dignissim finibus ac sit amet magna.
+                </p>
+                <h3 className={style.author}>Tim Smith</h3>
+            </div>
+
         </div>
     </>
     )
