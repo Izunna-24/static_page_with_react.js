81ad779ce3af29ef26ca8d87dd8f4ad0b7b947df
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-06-07 07:01:37 +0100

    react is kinda reacting

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,20 @@
 import Hero from "./Hero";
 import Clients from "./Clients";
+import MidSection1 from "./midSection1";
+import MemberSection from "./memberSection";
+import MidSection2 from "./midSection2";
+import MidSection3 from "./midSection3";
 
 const Home = () => {
     return (
         <>
             <Hero/>
             <Clients/>
+            <MidSection1/>
+            <MemberSection/>
+            <MidSection2/>
+            <MidSection3/>
+
         </>
     )
 }
---
+++ src/Pages/Home/midSection3/index.jsx
@@ -0,0 +1,25 @@
+import image from '../../../asset/teslalogo.png'
+import style from './index.module.css'
+
+const midSection3 = () =>{
+    return(
+    <>
+        <div className={style.container}>
+            <img src={image} alt="tesla logo" className={style.teslaLogo}/>
+            <p className={style.teslaText}>
+                Maecenas dignissim justo eget nulla rutrum molestie. Maecenas lobortis sem dui, <br/>
+                vel rutrum risus tincidunt ullamcorper. Proin eu enim metus. Vivamus sed libero ornare,<br/>
+                tristique quam in, gravida enim. Nullam ut molestie arcu, at hendrerit elit. Morbi laoreet <br/>
+                elit at ligula molestie, nec molestie mi blandit. Suspendisse cursus tellus sed augue <br/>
+                ultrices, quis tristique nulla sodales. Suspendisse eget lorem eu turpis vestibulum
+                pretium. <br/>
+                Suspendisse potenti. Quisque malesuada enim sapien, vitae placerat ante feugiat eget. <br/>
+                Quisque vulputate odio neque, eget efficitur libero condimentum id. Curabitur id nibh id <br/>
+                sem dignissim finibus ac sit amet magna.
+            </p>
+            <h3 style={{color: "#4caf4f"}}>Tim Smith</h3>
+        </div>
+    </>
+    )
+}
+export default midSection3;
