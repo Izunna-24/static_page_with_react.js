ebba58ad14c965b7bae3e2bd214ba0c7c5ebf3fa
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-06-07 07:01:09 +0100

    react is kinda reacting

---
+++ src/Pages/Home/midSection2/index.jsx
@@ -0,0 +1,19 @@
+import MidSection from "../../../component/midSection";
+import image from "./../../../asset/pana.png"
+const midSection2 = () =>{
+    return(
+
+       <MidSection title={"How to design your site footer like we did"}image={image}
+                   content={"Donec a eros justo. Fusce egestas tristique ultrices. " +
+                       "Nam tempor, augue nec tincidunt molestie, massa nunc varius arcu, at scelerisque elit erat a magna." +
+                       " Donec quis erat at libero ultrices mollis. In hac habitasse platea dictumst. " +
+                       "Vivamus vehicula leo dui, at porta nisi facilisis finibus. In euismod augue vitae nisi ultricies, non aliquet urna tincidunt." +
+                       " Integer in nisi eget nulla commodo faucibus efficitur quis massa. " +
+                       "Praesent felis est, finibus et nisi ac, hendrerit venenatis libero. " +
+                       "Donec consectetur faucibus ipsum id gravida."}/>
+
+
+    )
+
+}
+export default midSection2;
